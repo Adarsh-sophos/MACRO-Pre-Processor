4e887f8ce719aa2c5f40a26e24efd808368e3f64
Adarsh Jain <adarshjain583@gmail.com>
2017-03-12 15:05:15 +0530

    operator overloading added

--- replace_multi.py
+++ replace_multi.py
@@ -1,7 +1,7 @@
 import settings as st
 from tokens import create_tokens
 
-def replace_multi_line_macro(actual_par, i, prnt, key, lines):
+def replace_multi_line_macro(actual_par, i, prnt, key, lines, n_index):
     
     tab_len = space_len = 0
     
@@ -25,7 +25,7 @@ def replace_multi_line_macro(actual_par, i, prnt, key, lines):
     lines.pop(i)
     
     #get the line range of macro
-    def_r = st.macro_def_table.get(key)
+    def_r = st.macro_def_table.get(key)[n_index]
     
     if(len(def_r) == 1):
         j = def_r[0]
@@ -74,13 +74,31 @@ def create_parameter_table(key, s):
     #split to get each arguments passed
     p = s.split(",")
     
-    #check for number of positional arguments
-    n_of_pos_par = st.macro_name_table.get(key)[1][0]
+    key_arg = pos_arg = 0
     
     if(s==""):
         len_p = 0
     else:
         len_p = len(p)
+    
+    if(len_p != 0):
+        for qs in p:
+            if("=" in qs):
+                key_arg+=1
+            else:
+                pos_arg+=1
+
+    name_tab = st.macro_name_table.get(key)
+    def_n = name_tab[0][0]
+    if(def_n > 1):
+        for iq in range(1,def_n+1):
+            if(name_tab[iq][0] == pos_arg):
+                name_index = iq
+    else:
+        name_index = 1
+    
+    #check for number of positional arguments
+    n_of_pos_par = st.macro_name_table.get(key)[name_index][0]   
     
     if( n_of_pos_par > len_p ):
         print("Invalid no of arguments for macro "+key)
@@ -99,7 +117,7 @@ def create_parameter_table(key, s):
             opt_arg -= 1
     
     #get the parameter table for macro
-    par_list = st.parameter_name_table.get(key) 
+    par_list = st.parameter_name_table.get(key)[name_index-1] 
     
     #for every key in returned parameter table
     for i in range(len(par_list)):
@@ -112,7 +130,7 @@ def create_parameter_table(key, s):
         if(var_key in apt):
             continue
         
-        #if key's value in defination if None, then store the called argument
+        #if key's value in defination is None, then store the called argument
         if( var_value == None ):
             apt[var_key] = p[i].strip()
         
@@ -124,7 +142,7 @@ def create_parameter_table(key, s):
                 apt[var_key] = p[i].strip()
     
     #return the formed parameter table
-    return apt
+    return [apt,name_index-1]
 
 
 def nested_macro(m,n,lines,prnt):
@@ -153,10 +171,10 @@ def nested_macro(m,n,lines,prnt):
                     e_i = prnt[i].index(")", s_i)
                     
                     #get the actual parameter table for macro-call statement
-                    actual_par = create_parameter_table(key, prnt[i][s_i+1:e_i])
+                    actual_par, n_index = create_parameter_table(key, prnt[i][s_i+1:e_i])
                     
                     #replace macro with its defination
-                    ab = replace_multi_line_macro(actual_par, i, prnt, key, lines)
+                    ab = replace_multi_line_macro(actual_par, i, prnt, key, lines, n_index)
                     total_n += ab
                     cd = nested_macro(m,ab+1,lines,prnt)
                     total_n += cd
@@ -165,7 +183,7 @@ def nested_macro(m,n,lines,prnt):
                 
                 #else macro is single line
                 else:
-                    s_i = st.macro_def_table.get(key)[0]
+                    s_i = st.macro_def_table.get(key)[0][0]
                     prnt[i] = prnt[i].replace(key, prnt[s_i][prnt[s_i].index(key)+1:-1])
             k += 1
     
